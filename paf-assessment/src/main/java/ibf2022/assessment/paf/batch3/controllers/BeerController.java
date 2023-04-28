0eb198ba46edc25728036963ec14896290fa4982
cyeow <cheryl.ylt@gmail.com>
2023-04-28 13:27:01 +0800

    completed task 5

--- paf-assessment/src/main/java/ibf2022/assessment/paf/batch3/controllers/BeerController.java
+++ paf-assessment/src/main/java/ibf2022/assessment/paf/batch3/controllers/BeerController.java
@@ -1,5 +1,7 @@
 package ibf2022.assessment.paf.batch3.controllers;
 
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Optional;
 
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import ibf2022.assessment.paf.batch3.models.Brewery;
+import ibf2022.assessment.paf.batch3.models.LineItem;
 import ibf2022.assessment.paf.batch3.models.Order;
 import ibf2022.assessment.paf.batch3.repositories.BeerRepository;
 import ibf2022.assessment.paf.batch3.services.BeerService;
@@ -26,42 +29,55 @@ public class BeerController {
 	@Autowired
 	private BeerRepository repo;
 
-	//TODO Task 2 - view 0
-	@GetMapping(path="/")
+	// TODO Task 2 - view 0
+	@GetMapping(path = "/")
 	public String goToLanding(Model model) {
 		model.addAttribute("styles", repo.getStyles());
 		return "view0";
 	}
 
-	//TODO Task 3 - view 1
-	@GetMapping(path="/beer/style/{styleId}")
+	// TODO Task 3 - view 1
+	@GetMapping(path = "/beer/style/{styleId}")
 	public String browseByStyle(@PathVariable Integer styleId, @RequestParam String styleName, Model model) {
 		model.addAttribute("breweries", repo.getBreweriesByBeer(styleId));
 		model.addAttribute("styleName", styleName);
 		return "view1";
 	}
 
-	//TODO Task 4 - view 2
-	@GetMapping(path="/brewery/{breweryId}")
+	// TODO Task 4 - view 2
+	@GetMapping(path = "/brewery/{breweryId}")
 	public String goToBrewery(@PathVariable Integer breweryId, Model model) {
 		Optional<Brewery> optB = repo.getBeersFromBrewery(breweryId);
-		if(optB.isPresent()) {
+		if (optB.isPresent()) {
 			model.addAttribute("brewery", optB.get());
 		} else {
 			model.addAttribute("brewery", null);
 		}
 		return "view2";
 	}
-	
-	//TODO Task 5 - view 2, place order
-	@PostMapping(path="/brewery/{breweryId}/order", consumes=MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-	public String placeOrder(@PathVariable Integer breweryId, @RequestParam HashMap<Integer, Integer> orderMap, Model model) {
-		// parse order
+
+	// TODO Task 5 - view 2, place order
+	@PostMapping(path = "/brewery/{breweryId}/order", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public String placeOrder(@PathVariable Integer breweryId, @RequestParam HashMap<String, String> orderMap,
+			Model model) {
+		// parse order header
 		Order o = new Order();
 		o.setBreweryId(breweryId);
-		 
+		o.setDate(LocalDate.now());
 
-		model.addAttribute("orderId", svc.placeOrder(o));
+		// parse order details
+		// orderMap.keySet().forEach(key -> System.out.println(key.getClass()));
+		orderMap.keySet().forEach(beerId -> {
+			if (!orderMap.get(beerId).isBlank() && Integer.parseInt(orderMap.get(beerId)) > 0) {
+				o.getOrders().add(
+						new LineItem(Integer.parseInt(beerId), Integer.parseInt(orderMap.get(beerId))));
+			}
+		});
+
+		// place order and assign orderId
+		o.setOrderId(svc.placeOrder(o));
+
+		model.addAttribute("orderId", o.getOrderId());
 		return "view3";
 	}
 
--- paf-assessment/src/main/java/ibf2022/assessment/paf/batch3/repositories/OrderRepository.java
+++ paf-assessment/src/main/java/ibf2022/assessment/paf/batch3/repositories/OrderRepository.java
@@ -3,9 +3,11 @@ package ibf2022.assessment.paf.batch3.repositories;
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
 
 import ibf2022.assessment.paf.batch3.models.Order;
 
+@Repository
 public class OrderRepository {
 
 	@Autowired
